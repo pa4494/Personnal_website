a4cf4af83819ca1a5be6e6ad200782bc21cd689f
P-A Heurtebize <b00270721@essec.edu>
2019-08-12 22:43:54 +0200

    supervised ML page

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+from typing import Dict, Union
+
 from flask import Flask, url_for, render_template, send_file, request, redirect, jsonify, session, make_response
 from werkzeug import secure_filename
 from pandas.api.types import is_numeric_dtype
@@ -6,6 +8,8 @@ import seaborn as sns
 import os
 from pathlib import Path
 import matplotlib.pyplot as plt
+import inspect
+import pprint
 import csv
 
 app = Flask(__name__)
@@ -20,8 +24,8 @@ ALLOWED_EXTENSIONS = set(['txt', 'csv', 'png', 'jpg', 'jpeg', 'gif'])
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 from sklearn.linear_model import LinearRegression, LogisticRegression, Lasso, Ridge
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier, DecisionTreeRegressor
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.naive_bayes import GaussianNB
 from sklearn.svm import SVC
 
@@ -29,34 +33,48 @@ dic_models = {"Linear_Regression" : LinearRegression,
               "Logistic_Regression" : LogisticRegression,
               "Lasso_Regression" : Lasso,
               "Ridge_Regression" : Ridge,
-              "DecisionTree" : DecisionTreeClassifier,
+              "Decision_Tree_Regressor" : DecisionTreeRegressor,
+              "Random_Forest_Regressor": RandomForestRegressor,
+              "Decision_Tree_Classifier" : DecisionTreeClassifier,
+              "Random_Forest_Classifier" : RandomForestClassifier,
               "Gaussian_Naive_bayes" : GaussianNB,
               "Support_Vector_Machine" : SVC
               }
 
-list_dummy = ["Logistic_Regression",
-            "Lasso_Regression" ,
-            "Ridge_Regression" ,
-            "Gaussian_Naive_bayes" ,
-            "Support_Vector_Machine"]
+list_dummy = ["Linear_Regression",
+              "Logistic_Regression",
+              "Lasso_Regression" ,
+              "Ridge_Regression" ,
+              "Gaussian_Naive_bayes" ,
+              "Support_Vector_Machine"]
 
 list_norm = ["Logistic_Regression",
-            "Lasso_Regression" ,
-            "Ridge_Regression" ,
-            "DecisionTree" ,
-            "Gaussian_Naive_bayes" ,
-            "Support_Vector_Machine"]
+             "Lasso_Regression" ,
+             "Ridge_Regression" ,
+             "Decision_Tree" ,
+             "Gaussian_Naive_bayes" ,
+             "Support_Vector_Machine"]
 
+list_regr = ["Linear_Regression",
+             "Logistic_Regression",
+             "Lasso_Regression",
+             "Ridge_Regression",
+             "Decision_Tree_Regressor",
+             "Random_Forest_Regressor"]
 
+list_classif = ["Decision_Tree_Classifier",
+                "Random_Forest_Classifier",
+                "Gaussian_Naive_bayes",
+                "Support_Vector_Machine"]
 
 # Functions we want to use in html template using jinja2
 @app.context_processor
 def utility_processor():
     def name_file(file_path):
         return Path(file_path).stem
-    return dict(name_file=name_file, is_numeric= is_numeric_dtype, len = len, min = min)
+    return dict(name_file=name_file, is_numeric= is_numeric_dtype, len = len, min = min, str=str, type = type)
 
-# Indicate we do not want cache memory (Graphs are saved and showns as images. If not set, web app does not update graphs after form submission)
+# Indicates we do not want cache memory (Graphs are saved and shown as images. If not set, web app does not update graphs after form submission)
 @app.after_request
 def add_header(response):
     response.cache_control.max_age = 0
@@ -123,7 +141,7 @@ def univariate():
             for key in dic_graphs.keys():
                 if key != "none":
                     for column in dic_graphs.get(key):
-                        dic_func[key](dataset[column], ax=ax[nb_graphs])
+                        dic_func[key](dataset[column].dropna(), ax=ax[nb_graphs])
                         nb_graphs+=1
                         code+="<P> sns.{}(dataset[{}]))</P>".format(dic_func[key].__name__,column)
 
@@ -184,7 +202,7 @@ def bivariate():
 
             for x in x_list :
                 for y in y_list:
-                    graph_choice(x = x, y=y, data=dataset, ax=ax[nb_graphs])
+                    graph_choice(x = x, y=y, data=dataset.dropna(), ax=ax[nb_graphs])
                     nb_graphs+=1
                     code+="<P> sns.{graph_choice}(x = {x}, y={y}, data=dataset]))</P>".format(graph_choice = graph_choice.__name__, x=x, y=y)
 
@@ -194,13 +212,12 @@ def bivariate():
         response = make_response(render_template("bivariate.html", dataset = dataset, nb_options = nb_options, table_head = table_head, dic_graphs = dic_graphs, dic_func= dic_func, nb_graphs = nb_graphs, code = code))
         return response
 
-@app.route('/supervised_ML', methods = ["GET", "POST"])
+@app.route('/supervised_ML/', methods = ["GET", "POST"])
 def supervised_ML():
 
     code =""
     table_head=""
     has_null = False
-    list_models = {"Linear Regression": "reg", "Logistic Regression" : "Log"}
 
     # If no csv file has been uploaded, we redirect to the upload page
     if not session.get("file_path"):
@@ -215,44 +232,86 @@ def supervised_ML():
         code+="<p> dataset = pd.read_csv({})</p>".format(Path(session["file_path"]).name)
         table_head = dataset.head(10).to_html()
 
-        if request.method == "POST":
+    return render_template("supervised_ML.html", dataset = dataset, table_head = table_head, has_null = has_null, detail_null = detail_null, dic_models = dic_models)
 
-            # We delete columns based on user's choice
-            for column in list(request.form.getlist("delete")):
-                dataset.drop(column, axis=1, inplace=True)
+@app.route("/params", methods = ["POST"])
+def model_parameters():
 
-            # We treat null values
-            if request.form.get("treat_null") =="remove_rows":
-                dataset.dropna(axis=0, inplace=True)
-            else:
-                dataset.interpolate(method = "nearest", inplace= True)
+    session["model"] = request.form.get("model")
+    IP_address_user_= "A_DEFINIR"
+    dataset = pd.read_csv(session["file_path"])
+    help_model = dic_models[session["model"]].__doc__.split('\n')
+    # We delete columns based on user's choice
+    for column in request.form.getlist("delete"):
+        dataset.drop(column, axis=1, inplace=True)
 
-            # We dummify categorical variables
-            if request.form.get("model") in list_dummy:
-                dataset_preprocessed = pd.get_dummies(dataset, drop_first=True)
+    # We treat null values
+    if request.form.get("treat_null") == "remove_rows":
+        dataset.dropna(axis=0, inplace=True)
+    else:
+        dataset.interpolate(method="nearest", inplace=True)
 
-            # We split dataset between X and y
-            y = dataset[request.form.get("target")]
-            X = dataset.drop(request.form.get("target"), axis=1)
+    # We make sure target is expressed as a number
+    if not is_numeric_dtype(dataset[request.form.get("target")]):
+        print("APPLY ONE HOT ENCODER AND KEEP TABLE OF CORRESPONDING VALUE IN A TABLE")
 
-            # We split the dataset between train and test
-            from sklearn.model_selection import train_test_split
-            X_train, X_test, y_train, y_test = train_test_split(X, y,
-                                                                test_size=0.2,
-                                                                random_state=0,
-                                                                stratify=y) #ATTENTION. IF y has at least one class with a unique entry this will return an error
-            # We normalise
-            if request.form.get("model") in list_norm:
-                from sklearn.preprocessing import StandardScaler
-                sc_X = StandardScaler
-                X_train = sc_X.fit_transform(X_train)
-                X_test = sc_X.transform(X_test)
+    # We dummify categorical variables
+    if request.form.get("model") in list_dummy:
+        dataset = pd.get_dummies(dataset, drop_first=True)
 
-            # THEN WE SAVE EVERYTHING AND SEND USER ON NEXT PAGE WHERE HE CAN REFINE PARAMETERS OF THE MODEL
+    # We split dataset between X and y
+    y = dataset[request.form.get("target")]
+    X = dataset.drop(request.form.get("target"), axis=1)
 
-    return render_template("supervised_ML.html", dataset = dataset, table_head = table_head, has_null = has_null, detail_null = detail_null, list_models = list_models, dic_models = dic_models)
+    # We split the dataset between train and test
+    from sklearn.model_selection import train_test_split
+    X_train, X_test, y_train, y_test = train_test_split(X, y,
+                                                        test_size=0.2,
+                                                        random_state=0,
+                                                        stratify=y)  # ATTENTION. IF y has at least one class with a unique entry this will return an error
+    # We normalise
+    if request.form.get("model") in list_norm:
+        from sklearn.preprocessing import StandardScaler
+        sc_X = StandardScaler()
+        X_train = pd.DataFrame(sc_X.fit_transform(X_train), columns= X_train.columns)
+        X_test = pd.DataFrame(sc_X.transform(X_test), columns = X_test.columns)
 
-@app.route("/test_html", methods = ["GET", "POST"])
+    # We save a version of X and y with user IP address in each file name (to allow several users to use the tool at the same time)
+    for data, name in [(X_train, "X_train"), (X_test, "X_test"), (y_train, "y_train"),
+                       (y_test, "y_test")]:
+        path = os.path.join(uploads_dir, name + IP_address_user_ +".csv")
+        # To cover the case where data is a numpy, we apply pd.DataFrame
+        data.to_csv(path) # NEED TO ADD THE CODE TO GET THE IP ADDRESS
+        session[name+"_path"] = path
+
+    # We get a list of all the parameters taken by the model chosen by user
+    signature = inspect.getfullargspec(dic_models[session["model"]])
+    list_params = list(zip(signature.args[::-1], signature.defaults[::-1]))
+    list_params.sort()
+
+    return render_template("choice_model_parameters.html", list_params= list_params, help_model= help_model)
+
+@app.route("/result_model", methods = ["POST"])
+def result_model():
+    X_train = pd.read_csv(session["X_train_path"])
+    X_test = pd.read_csv(session["X_test_path"])
+    y_train = pd.read_csv(session["y_train_path"], names=["target"])
+    y_test = pd.read_csv(session["y_test_path"], names=["target"])
+
+    listo = request.form.getlist("params")
+    #for elem in request.form.getlist("params"):
+
+    signature = inspect.getfullargspec(dic_models[session["model"]])
+    dic_params = {}
+    for arg in signature.args[1:]:
+        dic_params[arg] = request.form.get(arg)
+
+    model = dic_models[session["model"]](**dic_params)
+    model.fit(X_train,y_train)
+
+    return render_template("result_model.html", model = model, X_train = X_train, X_test = X_test, y_train= y_train, y_test = y_test, dic_params=dic_params)
+
+@app.route("/test_html", methods = ["POST"])
 def test_html():
     var = "on est dans get"
     if request.method == "POST":
